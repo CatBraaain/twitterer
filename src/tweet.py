e01aeb7b1fc22a83d969f207018c14a4e64c12a6
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2024-03-11 11:10:02 +0900

    feat: nest tweet class

--- src/tweet.py
+++ src/tweet.py
@@ -1,5 +1,7 @@
 import pickle
 import re
+from dataclasses import dataclass, field
+from typing import List
 
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
@@ -20,13 +22,7 @@ class Tweet:
         self.url = "https://twitter.com" + (
             soup.select_one(Selector.ANALYTICS).get("href").removesuffix("/analytics")
         )
-        self.tweet_id = self.url.split("/")[-1]
-
-        user_elements = soup.select(Selector.USER_ELEMENTS)
-        self.user_name = user_elements[0].text
-        self.user_id = user_elements[1].text.removeprefix("@")
-
-        self.verified = bool(soup.select(Selector.VERIFIED))
+        self.id = self.url.split("/")[-1]
 
         try:
             self.date_time = soup.select_one(Selector.DATE_TIME).get("datetime")
@@ -34,26 +30,76 @@ class Tweet:
             self.date_time = ""
         self.is_ad = False if self.date_time else True
 
+        user_elements = soup.select(Selector.USER_ELEMENTS)
+        self.user = User(
+            name=user_elements[0].text,
+            id=user_elements[1].text.removeprefix("@"),
+            verified=bool(soup.select(Selector.VERIFIED)),
+        )
+
         content_elements = soup.select(Selector.CONTENT)
         extractor_map = {"span": (lambda e: e.text), "img": (lambda e: e.get("alt"))}
         self.content = "".join([extractor_map[e.name](e) for e in content_elements])
 
-        self.replys = re.sub(
-            "[^\\d]", "", soup.select_one(Selector.REPLYS).get("aria-label")
-        )
-        self.retweets = re.sub(
-            "[^\\d]", "", soup.select_one(Selector.RETWEETS).get("aria-label")
-        )
-        self.likes = re.sub(
-            "[^\\d]", "", soup.select_one(Selector.LIKES).get("aria-label")
-        )
-        self.analytics = re.sub(
-            "[^\\d]", "", soup.select_one(Selector.ANALYTICS).get("aria-label")
+        self.stat = Stat(
+            replys=re.sub(
+                "[^\\d]", "", soup.select_one(Selector.REPLYS).get("aria-label")
+            ),
+            retweets=re.sub(
+                "[^\\d]", "", soup.select_one(Selector.RETWEETS).get("aria-label")
+            ),
+            likes=re.sub(
+                "[^\\d]", "", soup.select_one(Selector.LIKES).get("aria-label")
+            ),
+            analytics=re.sub(
+                "[^\\d]", "", soup.select_one(Selector.ANALYTICS).get("aria-label")
+            ),
         )
 
-        self.img_urls = [e.get("src") for e in soup.select(Selector.IMGS)]
-        # self.img_cnt = len(self.img_urls)
-        # self.has_img = bool(self.img_cnt)
+        self.media = Media(
+            img=Img(
+                count=len(soup.select(Selector.IMGS)),
+                urls=[e.get("src") for e in soup.select(Selector.IMGS)],
+            ),
+            video=Video(
+                count=len(soup.select(Selector.VIDEOS)),
+                thumbnails=[
+                    e.get("src") for e in soup.select(Selector.VIDEO_THUMBNAILS)
+                ],
+            ),
+        )
 
-        self.video_cnt = len(soup.select(Selector.VIDEOS))
-        # self.has_video = bool(self.video_cnt)
+
+@dataclass
+class User:
+    name: str
+    id: str
+    verified: bool
+
+
+@dataclass
+class Stat:
+    replys: int
+    retweets: int
+    likes: int
+    analytics: int
+
+
+class Media:
+    def __init__(self, img, video):
+        self.has_img = bool(img.count)
+        self.img = img
+        self.has_video = bool(video.count)
+        self.video = video
+
+
+@dataclass
+class Img:
+    count: int
+    urls: List[str]
+
+
+@dataclass
+class Video:
+    count: int
+    thumbnails: List[str]
